af55380d7523321c5045a41af64d0281f8e9167c
Bala Ganesh <balaganeshbaskar@gmail.com>
2025-05-08 13:49:50 +0530

    Gazebo update

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,48 +1,36 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-
-import xacro
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import FindExecutable
 
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('articubot_one'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
+    # Dynamically build robot_description from xacro
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name='xacro')]),
+        ' ',
+        PathJoinSubstitution([FindPackageShare("articubot_one"), "description", "robot.urdf.xacro"])
+    ])
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[robot_description, {"use_sim_time": use_sim_time}],
+        output="screen"
     )
 
-    node_joint_state_publisher = Node(
-    package='joint_state_publisher',
-    executable='joint_state_publisher',
-    name='joint_state_publisher',
-    output='screen'
-)
-
-
-    # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-        node_joint_state_publisher,
-        node_robot_state_publisher
+            name="use_sim_time",
+            default_value="false",
+            description="Use simulation (Gazebo) clock if true"
+        ),
+        robot_state_publisher_node
     ])
